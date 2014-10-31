66902f37e0e690ce7e04a81ee2e629ad208c94d2
Basti <muellers@fh-brandenburg.de>
2014-10-31 11:29:20 +0100

    Works. If there are to much different components the number of colors is not sufficient to display each component with a unique color. Currently 10 components can have a distinct color.

--- src/main/java/de/fhb/graph/gModel/Algorithmen.java
+++ src/main/java/de/fhb/graph/gModel/Algorithmen.java
@@ -8,29 +8,40 @@ import java.util.Stack;
 public class Algorithmen {
 	
 	static int colour = 0;
-	
+
 	public static void findComponents(Graph g){
-		colour = colour+1;
+		colour = 0;
 		Stack<Vertex> agenda = new Stack<Vertex>();
 		HashSet<Vertex> vertices = g.getVertices();
-		if (vertices.isEmpty()) return;
-		Iterator<Vertex> it = vertices.iterator();
-		Vertex first = it.next();
-		while (first.isMarked()) {
-			if (!it.hasNext()) return;
-			first = it.next();
-		}
-		agenda.push(first);
-		while (!agenda.isEmpty()){
-			Vertex actual = agenda.pop();
-			if (!actual.isMarked()){
-				actual.setMarked(true);
-				actual.setColour(colour);
-				for (Vertex v : actual.getNeighbours()){
-					if (!agenda.contains(v)) agenda.push(v);
-				}
-			}
-		}
+
+        if (vertices.isEmpty()){
+            return;
+        }
+
+        Iterator<Vertex> it = vertices.iterator();
+		Vertex first;
+
+        while(it.hasNext())
+        {
+            first = it.next();
+            if(!first.isMarked()){
+                agenda.push(first);
+                colour +=1;
+                System.out.println("Colour: " + colour);
+                while (!agenda.isEmpty()){
+                    Vertex actual = agenda.pop();
+                    if (!actual.isMarked()){
+                        actual.setMarked(true);
+                        actual.setColour(colour);
+                        for (Vertex v : actual.getNeighbours()){
+                            if (!agenda.contains(v)){
+                                agenda.push(v);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
 
 	}
--- src/main/java/de/fhb/graph/gView/Config.java
+++ src/main/java/de/fhb/graph/gView/Config.java
@@ -7,7 +7,7 @@ public class Config {
 	public  static final Color VERTEXCOLOUR = Color.darkGray;
 	public  static final Color ACTIVEVERTEXCOLOUR = Color.RED;
 	public  static final Color EDGECOLOUR = Color.darkGray;
-	public static final Color[] COLOURS = {Color.darkGray, Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+	public static final Color[] COLOURS = {Color.darkGray, Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.PINK, Color.CYAN, Color.BLACK, Color.ORANGE};
 	public  static final int VERTEXRADIUS = 5;
 	
 	public static final String FILENAMESUFFIX = ".grf";
